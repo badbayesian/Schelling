75fa43c393e76953f49243cdd18a8f3c447c136e
Daniel Silva-Inclan <badbayesian@gmail.com>
2017-12-28 18:26:36 -0500

    ENH: simple schelling seg app

--- r_version/schelling/app.R
+++ r_version/schelling/app.R
@@ -1,59 +1,64 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+
+   titlePanel("Schelling Segregation Model"),
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         sliderInput(inputId = "height",
+                     label = "Height",
+                     min = 10,
+                     max = 100,
+                     value = 50),
+         sliderInput(inputId = "width",
+                     label = "Width",
+                     min = 10,
+                     max = 100,
+                     value = 100),
+         sliderInput(inputId = "tolerence",
+                     label = "Tolerence",
+                     min = 0,
+                     max = 1,
+                     value = 0.33),
+         actionButton(
+           inputId = "submit_loc",
+           label = "Submit")
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("schelling_plot")
       )
    )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
   source(file = paste0(getwd(), "/schelling.R"))
   
-  grid <- schelling()
-
-   output$distPlot <- renderPlot({
-
-     
-     plot <- ggplot(grid) +
-       aes(x = width, y = height, color = as.factor(race)) +
-       geom_point(size = 2.5) +
-       labs(title = "Schelling", x = "", y = "", color = "Race") +
-       theme_bw() +
-       coord_cartesian(xlim = c(0, max(grid$width) + 1),
-                       ylim = c(0, max(grid$height) + 1),
-                       expand = FALSE)
-     print(plot)
-   })
+  observeEvent(
+    eventExpr = input[["submit_loc"]],
+    handlerExpr = {
+      print("PRESSED")
+  
+     output$schelling_plot <- renderPlot({
+       
+       grid <- schelling(height = input$height,
+                         width = input$width,
+                         tolerence = input$tolerence)
+  
+       plot <- ggplot(grid) +
+         aes(x = width, y = height, color = as.factor(race)) +
+         geom_point(size = 2.5) +
+         labs(title = "Schelling", x = "", y = "", color = "Race") +
+         theme_bw() +
+         coord_cartesian(xlim = c(0, max(grid$width) + 1),
+                         ylim = c(0, max(grid$height) + 1),
+                         expand = FALSE)
+       print(plot)
+       })
+    }
+  )
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
-
--- r_version/schelling/schelling.R
+++ r_version/schelling/schelling.R
@@ -5,21 +5,21 @@ library(utils)
 #' TODO: allow for arbitrary neighborhood function
 #' 
 #' @param grid DataFrame
-#' @param max_height int
-#' @param max_width int
+#' @param height int
+#' @param width int
 #' @return neighborhood list of neighbors at index i
-neighbors <- function(grid, max_height, max_width){
+neighbors <- function(grid, height, width){
   height_neighbors <- sapply(grid$height, function(i) seq(i - 1, i + 1))
   excess_height <- which((height_neighbors == 0) |
-                           (height_neighbors == max_height + 1), arr.ind = T)
+                           (height_neighbors == height + 1), arr.ind = T)
   height_neighbors[excess_height] <- NA
   
   width_neighbors <- sapply(grid$width, function(i) seq(i - 1, i + 1))
   excess_width <- which((width_neighbors == 0) |
-                          (width_neighbors == max_width + 1), arr.ind = T)
+                          (width_neighbors == width + 1), arr.ind = T)
   width_neighbors[excess_width] <- NA
   
-  neighborhood <- lapply(1:(max_height*max_width), function(i)
+  neighborhood <- lapply(1:(height*width), function(i)
     expand.grid(height = na.omit(height_neighbors[, i]),
                 width = na.omit(width_neighbors[, i])))
   
@@ -30,22 +30,22 @@ neighbors <- function(grid, max_height, max_width){
 #' `tolerance`.
 #'
 #' @param grid DataFrame
-#' @param max_height int
-#' @param max_width int
+#' @param height int
+#' @param width int
 #' @param tolerance float
 #' @return satisfied list of bools
-race_check <- function(grid, max_height, max_width, tolerance){
-  neighborhood <- neighbors(grid, max_height, max_width)
+race_check <- function(grid, height, width, tolerance){
+  neighborhood <- neighbors(grid, height, width)
   
   # TODO optimize with sparse matrix multiplication
-  race_counts <- lapply(1:(max_height*max_width), function(i)
+  race_counts <- lapply(1:(height*width), function(i)
     grid$height %in% neighborhood[[i]]$height *
       grid$width %in% neighborhood[[i]]$width *
       grid$race)
-  race_counts <- lapply(1:(max_height*max_width), function(i)
+  race_counts <- lapply(1:(height*width), function(i)
     table(race_counts[[i]][race_counts[[i]] != 0]))
 
-  satisfied <- sapply(1:(max_height*max_width), function(i)
+  satisfied <- sapply(1:(height*width), function(i)
     tolerance <= ((race_counts[[i]][toString(grid$race[i])] - 1) /
                     sum(race_counts[[i]])))
 
@@ -54,22 +54,22 @@ race_check <- function(grid, max_height, max_width, tolerance){
 
 #' Initialize and run Schelling process
 #'
-#' @param max_height int
-#' @param max_width int
+#' @param height int
+#' @param width int
 #' @param tolerence float
 #' @param max_iterations int
 #' @param satisfied_agents float
 #' @return grid
-schelling <- function(max_height = 50, max_width = 100, tolerence = 0.33,
+schelling <- function(height = 50, width = 100, tolerence = 0.33,
                       max_iterations = 100, satisfied_agents = 0.95){
-  number_of_agents <- max_height*max_width
+  number_of_agents <- height*width
   
-  grid <- data.frame(expand.grid(height = 1:max_height, width = 1:max_width),
+  grid <- data.frame(expand.grid(height = 1:height, width = 1:width),
                      empty = FALSE,
                      satisfied = FALSE,
                      race = sample(3, number_of_agents, replace = TRUE))
   for (i in 1:max_iterations) {
-    grid$satisfied <- race_check(grid, max_height, max_width, tolerence)
+    grid$satisfied <- race_check(grid, height, width, tolerence)
     
     if (sum(grid$satisfied)/(number_of_agents) >= satisfied_agents) {
       break
